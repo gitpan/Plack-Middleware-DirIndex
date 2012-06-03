20a17c7c2b7c5b584d16e5035e5e2ef603e3e07c
Leo Lapworth <LLAP@cuckoo.org>
2012-06-03 15:07:11 +0000

    Import of LLAP/Plack-Middleware-DirIndex-0.01 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-DirIndex
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         LLAP/Plack-Middleware-DirIndex-0.01.tar.gz
    gitpan-cpan-author:       LLAP
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,5 @@
+plack-middleware-dirindex
+=========================
+
+Middleware to use with Plack::App::Directory and the like
+
---
+++ lib/Plack/Middleware/DirIndex.pm
@@ -0,0 +1,51 @@
+package Plack::Middleware::DirIndex;
+{
+  $Plack::Middleware::DirIndex::VERSION = '0.01';
+}
+
+# ABSTRACT: Append an index file to request PATH's ending with a /
+
+use parent qw( Plack::Middleware );
+use Plack::Util::Accessor qw(dir_index);
+
+=head1 NAME
+
+Plack::Middleware::DirIndex
+
+=head1 SYNOPSIS
+
+  use Plack::Builder;
+  use Plack::App::File;
+  use Plack::Middleware::DirIndex;
+
+  my $app = Plack::App::File->new({ root => './htdocs/' })->to_app;
+
+  builder {
+        enable "Plack::Middleware::DirIndex", dir_index => 'index.html';
+        $app;
+  }
+  
+=head1 DESCRIPTION
+
+If $env->{PATH_INFO} ends with a '/' then we will append the dir_index
+value to it (defaults to index.html)
+
+=cut
+
+sub prepare_app {
+    my ($self) = @_;
+
+    $self->dir_index('index.html') unless $self->dir_index;
+}
+
+sub call {
+    my ( $self, $env ) = @_;
+
+    if ( $env->{PATH_INFO} =~ m{/$} ) {
+        $env->{PATH_INFO} .= $self->dir_index();
+    }
+
+    return $self->app->($env);
+}
+
+1;
